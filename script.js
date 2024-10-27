98921ebc046b633b8a52faaf2a7995b5374f9d02
Calyx21 <ritztzy@gmail.com>
2024-10-27 15:12:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleSlide() {
+    const memorySlide = document.getElementById('memory-slide');
+    memorySlide.classList.toggle('visible');
+    memorySlide.classList.toggle('hidden');
+}
